b8ea1d7ea4f47f87a990e2be26efee064a2fc924
VarshiniJeremy <159221312+VarshiniJeremy@users.noreply.github.com>
2024-03-12 13:54:19 +0530

    Create DIFFINH.JAVA

---
+++ DIFFINH.JAVA
@@ -0,0 +1,44 @@
+//Base Class
+class College {
+    College() {
+        System.out.println("College");
+    }
+}
+
+// Simple Inheritance
+class Department extends College {
+
+    Department() {
+        System.out.println("Department inherits College");
+    }
+}
+
+// Multilevel Inheritance
+// Here Student class inherits Department class(Hierarchical Inheritance)
+class Student extends College {
+
+    Student() {
+        System.out.println("Student inherits college");
+    }
+}
+
+// Here Teacher class inherits Department class(Hierarchical Inheritance)
+class Teacher extends College {
+    Teacher() {
+        System.out.println("Teacher inherits college");
+    }
+}
+
+class InheritanceTypes {
+    public static void main(String[] args) {
+        System.out.println("****Simple Inheritance****");
+        Department simple = new Department();
+        System.out.println();
+        System.out.println("****Multilevel Inheritance****");
+        Student Multilevel = new Student();
+        System.out.println();
+        System.out.println("****Hierarchical Inheritance****");
+        Student hierarchical1 = new Student();
+        Teacher hierarchical2 = new Teacher();
+    }
+}
